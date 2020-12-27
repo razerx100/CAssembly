8df1b892e09e0eaec0f5641bc6d747557770aeeb
Saikat <saikatmandal8@gmail.com>
2020-12-27 14:38:30 +0600

    made addVec to return and better Vec4 class

--- addVector4.asm
+++ addVector4.asm
@@ -1,8 +1,8 @@
     global      AddVec4
     section     .text
 AddVec4:
-    movdqa      xmm0, [rdi]     ;FIRST PARAMETER IN RDI
-    movdqa      xmm1, [rsi]     ;SECOND PARAMETER IN RSI
-    addps       xmm0, xmm1      ;4x4 FLOAT ADD IN SINGLE INSTRUCTION
-    movdqa      [rdx], xmm0     ;MOVE RESULT IN THIRD PARAMETER
+    movdqa      xmm0, [rdi]
+    movdqa      xmm1, [rsi]
+    addps       xmm0, xmm1
+    movdqa      [rax], xmm0
     ret
--- addVector4.cpp
+++ addVector4.cpp
@@ -1,16 +1,30 @@
 #include<iostream>
-extern "C" void AddVec4(float* var1, float* var2, float* result);
+extern "C" float* AddVec4(float* var1, float* var2);
 struct Vec4 {
     float x;
     float y;
     float z;
     float w;
+    Vec4(){}
+    Vec4(float a, float b, float c, float d)
+        : x(a), y(b), z(c), w(d) {}
+    Vec4(float* packed)
+        : x(*packed), y(*(packed + 1)), z(*(packed + 2)), w(*(packed + 3)) {}
+    Vec4& operator=(float* packed){
+        this->x = *packed;
+        this->y = *(packed + 1);
+        this->z = *(packed + 2);
+        this->w = *(packed + 3);
+        return *this;
+    }
+    float* operator+(Vec4 other){
+        return AddVec4(reinterpret_cast<float*>(this), reinterpret_cast<float*>(&other));
+    }
 };
 int main(){
     Vec4 var1 = {1.2f, 3.5f, 0.6f, 19.1f};
     Vec4 var2 = {0.8f, 1.5f, 2.4f, 0.9f};
-    Vec4 result = {};
-    AddVec4((float*)&var1, (float*)&var2, (float*)&result);
+    Vec4 result = var1 + var2;
     std::cout << "X = " << result.x << " Y = " << result.y << "\n Z = " << result.z
     << " W = " << result.w;
     return 0;
